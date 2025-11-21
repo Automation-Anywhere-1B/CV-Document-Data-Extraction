51694fc17951195b9dcd51ff7e73e2ccae2091d2
margoglvz <msgalvez06@gmail.com>
2025-11-20 16:17:50 -0800

    added instructions and image upload

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,9 +2,7 @@ from streamlit_option_menu import option_menu
 import streamlit as st
 import numpy as np
 import pandas as pd
-
-# Title
-st.title(":red[_Welcome to CalVision_]")
+from io import StringIO
 
 # Sidebar menu
 with st.sidebar:
@@ -18,3 +16,41 @@ with st.sidebar:
       }
     )
     selected
+
+# Title
+st.title(":red[_Welcome to CalVision_]")
+
+# Subheader
+st.subheader("Instructions")
+
+# Instructions Text
+st.markdown("**Welcome!**\nIn order to submit your photo for parsing, please make sure to follow these simple instructions.")
+st.markdown(":one: Position your check in the center of your camera")
+st.markdown(":two: Make sure you have good lighting and the **_whole_** check is visible")
+st.markdown(":three: Confirm that your check is in focus and there are no other objects in the photo")
+st.markdown(":four: You are ready to submit!!")
+
+
+
+# Uploading Image
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
+# Submit Button
+submit = st.button("Submit Your Photo", type="primary")
